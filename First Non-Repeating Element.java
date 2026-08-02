4747de48847ceb7674561f70f3193b990f4ffd5d
SuryaBalsamy <suryabalsamy@gmail.com>
2026-08-02 10:15:09 +0530

    FirstNon-RepeatingElement.java

---
+++ First Non-Repeating Element.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0; i<n; i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0; i<arr.length; i++)
+        {
+            int count=1;
+            for(int j=0; j<arr.length; j++)
+            {
+                if(arr[i]==arr[j] && i!=j)
+                {
+                    count++;
+                }
+            }
+            if(count==1)
+            {
+                System.out.print(arr[i]);
+                return;
+            }
+        }
+        System.out.println(-1); 
+    }
+}
