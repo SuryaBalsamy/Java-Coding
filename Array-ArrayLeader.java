4bffcc9503d93f87b222f92e779e7d9c27e7a834
SuryaBalsamy <suryabalsamy@gmail.com>
2026-08-02 10:45:55 +0530

    Array-ArrayLeader.java

---
+++ Array-ArrayLeader.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0; i<n; i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0; i<arr.length; i++)
+        {
+            boolean leader=true;
+            for(int j=i+1; j<arr.length; j++)
+            {
+                if(arr[j]>arr[i])
+                {
+                    leader=false;
+                    break;
+                }
+            }
+            if(leader)
+            {
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}
