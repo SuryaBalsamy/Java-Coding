b127fb54cba588e073b5943948f9c3521284d793
SuryaBalsamy <suryabalsamy@gmail.com>
2026-08-03 11:19:39 +0530

    Array-MaximumSumSubarray.java

---
+++ Array-MaximumSumSubarray.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int sum=0;
+        int max=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            sum+=arr[i];
+            if(sum<0)
+            {
+                sum=0;
+            }
+            if(sum>max)
+            {
+                max=sum;
+            }
+        }
+        System.out.print(max);
+    }
+}
