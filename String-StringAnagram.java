da23befe0cdf784d5602d525645f6f95db4b1632
SuryaBalsamy <suryabalsamy@gmail.com>
2026-08-06 18:07:17 +0530

    String-StringAnagram.java

---
+++ String-StringAnagram.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main 
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        if (s1.length() != s2.length()) 
+        {
+            System.out.println("Not Anagram");
+            return;
+        }
+        char a[] = s1.toCharArray();
+        char b[] = s2.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        if (Arrays.equals(a, b))
+            System.out.println("Anagram");
+        else
+            System.out.println("Not Anagram");
+    }
+}
